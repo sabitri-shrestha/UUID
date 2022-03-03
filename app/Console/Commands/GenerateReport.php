8c735f7660cf7679065ceb5f4e1c301f7d9d14b1
sabitri shrestha <sabitri.shrestha03@gmail.com>
2022-03-03 18:44:48 +1100

    .

--- app/Console/Commands/GenerateReport.php
+++ app/Console/Commands/GenerateReport.php
@@ -49,29 +49,18 @@ class GenerateReport extends Command
             $latest_student_response = collect($student_response_json)->where("student.id","=",$student_id)->sortByDesc('completed')->first();
        
             $questions_json = array_filter(json_decode(file_get_contents('./data/questions.json'),true));
+            $total_question = count(collect($questions_json));
             $correct_answers_count = 0;
             foreach($latest_student_response['responses'] as $res){
                 $question = collect($questions_json)->where('id','=',$res['questionId'])->where('config.key','=',$res['response']);
                 if(count($question) > 0){ $correct_answers_count++; };
             }
-             
-                
-              
-                
-            
-        
-            
- 
-
             if($choice == 1){
                 $f_name = $student_data[0]['firstName'];
                 $l_name =  $student_data[0]['lastName'];
                 $last_assesment_date = $latest_student_response['completed'];
-                
-
                 $this->info($f_name.' '.$l_name.' recently completed Numeracy assessment on '.$last_assesment_date.'
-                He got '.correct_answers_count.' questions right out of 16. Details by strand given below:
-                
+                He got '.$correct_answers_count.' questions right out of '.$total_question.'. Details by strand given below:
                 Numeracy and Algebra: 5 out of 5 correct
                 Measurement and Geometry: 7 out of 7 correct
                 Statistics and Probability: 3 out of 4 correct');
